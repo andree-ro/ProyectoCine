9b1e5239368d89396caa984945bef8f1fb99921d
Andreé Robles <andreerm14@gmail.com>
2025-04-20 19:43:28 -0600

    Eliminando

--- backend/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const authRoutes = require('./routes/authRoutes');
-const app = express();
-
-app.use('/api/auth', authRoutes);
-
-module.exports = app;
--- frontend/src/components/QRGenerator.js
+++
@@ -1,9 +0,0 @@
-// QRGenerator.js
-import QRCode from 'qrcode.react';
-
-const QRGenerator = ({ reservationId }) => (
-  <div>
-    <QRCode value={`ReservaID:${reservationId}`} size={128} />
-    <p>Escanea este código para ver los detalles de tu reserva.</p>
-  </div>
-);
